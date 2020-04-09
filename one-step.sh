47012e7ae93f22af31c0e4c1b7eb375c825be4fb
Joe Moore <protean@gmail.com>
2020-04-09 10:52:34 -0400

    Fixing NVM setup.

--- one-step.sh
+++ one-step.sh
@@ -1,4 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+source ~/.zshrc
 npm i -g spaceship-prompt
 
