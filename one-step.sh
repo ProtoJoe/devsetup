05a81c1d5cae97d29b6bc3bb643b82823f0a9054
Joe Moore <protean@gmail.com>
2020-04-09 10:55:57 -0400

    This isn't working...

--- one-step.sh
+++ one-step.sh
@@ -1,5 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
 source ~/.zshrc
+nvm install 10
 npm i -g spaceship-prompt
-
