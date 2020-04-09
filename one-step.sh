bff0ac792ae8305410ec97793b7eddc14d3fc546
Joe Moore <protean@gmail.com>
2020-04-09 10:49:03 -0400

    Adding NVM setup

--- one-step.sh
+++ one-step.sh
@@ -1,3 +1,4 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 npm i -g spaceship-prompt
 
